0d725140b8db0b68a2046219a84158144cf289af
dmolinsky <diana.molinsky@iths.se>
2026-01-04 11:33:51 +0100

    add: fonts and global css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import "@fontsource/inter/latin-400.css";
+import "@fontsource/inter/latin-500.css";
+import "@fontsource/inter/latin-600.css";
+import "@fontsource/chewy/latin-400.css";
+
+import "./styles/globals.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
